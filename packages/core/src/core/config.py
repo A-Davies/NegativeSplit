24d089dccf39ba9b820675182fc3b12a5c5c7e5e
VRT\adavies <adavies@visionrt.com>
2026-02-05 11:25:36 +0000

    added strava keys to be loaded from .env

--- packages/core/src/core/config.py
+++ packages/core/src/core/config.py
@@ -1,21 +1,30 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 
 class Config:
-    # 1. Find the Workspace Root (Climb up from this file)
+    # Find the Workspace Root (Climb up from this file)
     # config.py -> core -> src -> core -> projects -> NegativeSplit
     ROOT_DIR = Path(__file__).resolve().parent.parent.parent.parent.parent
 
-    # 2. Define Data Subdirectories
+    # Define Data Subdirectories
     DATA_DIR = ROOT_DIR / "data"
     RAW_DIR = DATA_DIR / "raw"
     GPX_DIR = RAW_DIR / "gpx"
     PROCESSED_DIR = DATA_DIR / "processed"
     CACHE_DIR = DATA_DIR / "cache"
 
-    # 3. Specific File Paths
+    # Specific File Paths
     PARQUET_PATH = PROCESSED_DIR / "activities.parquet"
     TOKEN_JSON = CACHE_DIR / "token.json"
+
+    # Strava API keys, get from environment
+    # Load .env file into environment variables
+    # TODO - add a try except here, this needs to be flagged when it fails, as well as how to fix.
+    load_dotenv()
+    STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID")
+    STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET")
 
     @classmethod
     def setup_folders(cls):
