ffb65219ec1159832d65da77a6beb3724f1aa272
VRT\adavies <adavies@visionrt.com>
2026-02-11 14:39:33 +0000

    testing if this can be committed

--- packages/strava-api/src/strava_api/token_manager.py
+++ packages/strava-api/src/strava_api/token_manager.py
@@ -1,3 +1,5 @@
+"""Manages Strava API token, including loading, saving, and refreshing."""
+
 import json
 import time
 from dataclasses import dataclass
@@ -16,15 +18,30 @@ class StravaToken(BaseModel):
 
 @dataclass
 class TokenManager:
+    """Manages Strava API token, including loading, saving, and refreshing.
+
+    :raises MissingStravaTokenError: raised when json does not exist.
+    """
+
     client_id: str
     client_secret: str
     cache_path: Path = settings.TOKEN_JSON
 
     def save_token(self, token_data: dict) -> None:
+        """Saves generated Strava token as a .json file.
+
+        :param token_data: dict with Strava token details.
+        :type token_data: dict
+        """
         Path(self.cache_path.parent).mkdir(exist_ok=True, parents=True)
         self.cache_path.write_text(json.dumps(token_data, indent=4))
 
     def load_token(self) -> StravaToken | None:
+        """Loads a Strava token from `self.cache_path` and returns a `StravaToken` object, or None.
+
+        :return: Strava tokens
+        :rtype: StravaToken | None
+        """
         if not self.cache_path.exists():
             return None
         with self.cache_path.open() as f:
@@ -33,7 +50,7 @@ class TokenManager:
     def get_valid_token(self) -> str:
         token = self.load_token()
         if not token:
-            raise Exception("No token found. Please run the OAuth login flow.")
+            raise MissingStravaTokenError
 
         # If token expires in less than 5 minutes, refresh it
         if token.expires_at < (time.time() + 300):
@@ -55,3 +72,7 @@ class TokenManager:
         new_token_data = response.json()
         self.save_token(new_token_data)
         return new_token_data["access_token"]
+
+
+class MissingStravaTokenError(Exception):
+    """Missing Strava Token File. credentials. Expected at `settings.TOKEN_JSON`."""
